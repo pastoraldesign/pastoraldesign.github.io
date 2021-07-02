b2e1f38ca9073c0d71393495d1c0a19ff06ecb86
Erfan Najafabadi <erfanna@gmail.com>
2021-07-03 02:31:41 +0430

    date

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,12 +31,12 @@
     /* ==========================================================================
     countdown timer
     ========================================================================== */
-     jQuery('#clock').countdown('2018/10/19',function(event){
+     jQuery('#clock').countdown('2021/08/03',function(event){
       var $this=jQuery(this).html(event.strftime(''
-      +'<div class="time-entry days"><span>%-D</span> Days</div> '
-      +'<div class="time-entry hours"><span>%H</span> Hours</div> '
-      +'<div class="time-entry minutes"><span>%M</span> Minutes</div> '
-      +'<div class="time-entry seconds"><span>%S</span> Seconds</div> '));
+      +'<div class="time-entry days"><span>%-D</span> روز</div> '
+      +'<div class="time-entry hours"><span>%H</span> ساعت</div> '
+      +'<div class="time-entry minutes"><span>%M</span> دقیقه</div> '
+      +'<div class="time-entry seconds"><span>%S</span> ثانیه</div> '));
     });
 
     /* WOW Scroll Spy
